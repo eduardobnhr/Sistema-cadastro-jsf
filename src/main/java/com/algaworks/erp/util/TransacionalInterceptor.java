5b696ce3a6550ec0ecb3b1f47431a2e4bb979b33
eduardobnhr <eduardobnhr@gmail.com>
2023-10-24 19:53:43 -0300

    injecao de dependencias

--- src/main/java/com/algaworks/erp/util/TransacionalInterceptor.java
+++ src/main/java/com/algaworks/erp/util/TransacionalInterceptor.java
@@ -10,44 +10,46 @@ import javax.interceptor.InvocationContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-
 @Interceptor
 @Transacional
 @Priority(Interceptor.Priority.APPLICATION)
-public class TransicionalInterceptor  implements Serializable{
+public class TransacionalInterceptor implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager manager;
-	
+
 	@AroundInvoke
-	public Object invoke(InvocationContext context) throws Exception{
+	public Object invoke(InvocationContext context) throws Exception {
 		EntityTransaction trx = manager.getTransaction();
-		
 		boolean criador = false;
+
 		try {
-			if(!trx.isActive()) {
-				//truque para fazer rollback no que ja passou
-				//senao um futuro commit confirmaria ate mesmo operacoes sem transacao
+			if (!trx.isActive()) {
+				// truque para fazer rollback no que já passou
+				// (senão, um futuro commit confirmaria até mesmo operações sem transação)
 				trx.begin();
 				trx.rollback();
-				//agr sim inicia a transicao
+
+				// agora sim inicia a transação
 				trx.begin();
+
 				criador = true;
 			}
+
 			return context.proceed();
-		}catch(Exception e) {
-			if(trx != null && criador) {
+		} catch (Exception e) {
+			if (trx != null && criador) {
 				trx.rollback();
 			}
+
 			throw e;
-		}finally {
+		} finally {
 			if (trx != null && trx.isActive() && criador) {
 				trx.commit();
 			}
 		}
-		
 	}
 
 }
-
